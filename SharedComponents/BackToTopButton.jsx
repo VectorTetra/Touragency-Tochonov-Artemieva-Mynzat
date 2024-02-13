2a65ba249639b4a648013dfc8a9aa66c37ceed23
VectorTetra <viktochonov@gmail.com>
2024-02-13 20:45:32 +0200

    Check Back To Top Button 3

--- SharedComponents/BackToTopButton.jsx
+++ SharedComponents/BackToTopButton.jsx
@@ -1,5 +1,9 @@
 function BackToTopButton(props){
 	return (
-		<button type="button" className="BackToTopButton">To Top</button>
+		<button type="button" className="BackToTopButton" 
+		onClick={() => {window.scroll({top:0,behavior:"smooth"})}}>To Top
+		
+		</button>
+
 	)
 }
