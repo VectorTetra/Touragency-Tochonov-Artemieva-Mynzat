33a1daa3cd931c751266e5cb5aa0d6ddc847c306
VectorTetra <viktochonov@gmail.com>
2024-02-13 21:49:27 +0200

    Trying to display Policy Text

--- Page Public contracts and offers/PrivacyPolicy.jsx
+++ Page Public contracts and offers/PrivacyPolicy.jsx
@@ -3,19 +3,17 @@ function PrivacyPolicy(props) {
 		<div id="policy-block">
 			{
 				props.Policy.map(item => {
-					switch (item.type) {
-						case "Article":
-							<PrivacyPolicyArticle articleData={item} />
-							break;
-						case "PlainText":
-							<section>
-								<h6>{item.caption}</h6>
-								<p>{item.text}</p>
-							</section>
-							break;
-						default:
-							<b>Nothing</b>
+					if (item.type === "Article") { <PrivacyPolicyArticle articleData={item} /> }
+					if (item.type === "PlainText") {
+						<section>
+							<h6>{item.caption}</h6>
+							<p>{item.text}</p>
+						</section>
 					}
+
+
+
+
 				})
 			}
 		</div>
