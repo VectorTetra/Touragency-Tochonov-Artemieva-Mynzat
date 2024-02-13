0430dcc98c5fc72800cf440a611451cf9bf7312e
VectorTetra <viktochonov@gmail.com>
2024-02-13 23:45:00 +0200

    Again, Policy and Articles was rebuilded

--- Page Public contracts and offers/PrivacyPolicy.jsx
+++ Page Public contracts and offers/PrivacyPolicy.jsx
@@ -5,15 +5,7 @@ function PrivacyPolicy(props) {
 				console.log("props policy",props.policy)
 			}
 			{
-				props.policy.map(item =>
-					{item.statType === "Article" ?  <PrivacyPolicyArticle articleData={item} />  :
-					 
-					<section>
-						<h6>{item.caption}</h6>
-						{
-							item.text.map(parag=> <p>{parag}</p>)
-						}
-					</section>}
+				props.policy.map(item => <PrivacyPolicyArticle articleData={item} />
 					// console.log(item);
 					// console.log(item.statType === "Article");
 					
--- Page Public contracts and offers/PrivacyPolicyArticle.jsx
+++ Page Public contracts and offers/PrivacyPolicyArticle.jsx
@@ -1,6 +1,7 @@
 function PrivacyPolicyArticle(props) {
-	
+
 	return (
+		item.statType === "Article" ? 
 		<section>
 			<h6>{props.caption}</h6>
 			<ul>
@@ -8,13 +9,20 @@ function PrivacyPolicyArticle(props) {
 					props.articleData.articleStatements.map(item => {
 						<li className="article"><b>{item.number}</b> {item.text}</li>
 						if (item.sublist !== null) {
-							<PrivacyPolicyArticleSublist sublist={item.sublist}/>
+							<PrivacyPolicyArticleSublist sublist={item.sublist} />
 						}
 					}
 					)
 				}
 			</ul>
-
+		</section> 
+		:
+		<section>
+			<h6>{item.caption}</h6>
+			{
+				item.text.map(parag => <p>{parag}</p>)
+			}
 		</section>
+
 	)
 }
