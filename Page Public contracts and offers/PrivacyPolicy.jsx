c32c355ea51df56f85f6ae465e97416bcfe59956
VectorTetra <viktochonov@gmail.com>
2024-02-13 23:02:15 +0200

    Again

--- Page Public contracts and offers/PrivacyPolicy.jsx
+++ Page Public contracts and offers/PrivacyPolicy.jsx
@@ -3,13 +3,13 @@ function PrivacyPolicy(props) {
 		<div id="policy-block">
 			{
 				props.policy.map(item => {
-					item.type === "Article" ?  <PrivacyPolicyArticle articleData={item} />  :
+					item.statType === "Article" ?  <PrivacyPolicyArticle articleData={item} />  :
 					 
 					<section>
 						<h6>{item.caption}</h6>
 						<p>{item.text}</p>
 					</section>;
-					console.log(item.type);
+					console.log(item.statType);
 					
 				})
 			}
