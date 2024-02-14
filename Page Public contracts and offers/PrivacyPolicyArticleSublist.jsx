746569d2ea2ce37e6b2092f53365588489571d88
VectorTetra <viktochonov@gmail.com>
2024-02-14 11:26:50 +0200

    Again

--- Page Public contracts and offers/PrivacyPolicyArticleSublist.jsx
+++ Page Public contracts and offers/PrivacyPolicyArticleSublist.jsx
@@ -2,9 +2,9 @@ function PrivacyPolicyArticleSublist(props) {
 	return (
 			<ul>
 				{
-					props.sublist.map(item => {
-						return <li key={item.number} className="subArticle"> <b>{item.number}</b>{item.text}</li>
-					}
+					props.sublist.map(item =>
+						<li key={item.number} className="subArticle"> <b>{item.number}</b>{item.text}</li>
+					
 					)
 				}
 			</ul>
