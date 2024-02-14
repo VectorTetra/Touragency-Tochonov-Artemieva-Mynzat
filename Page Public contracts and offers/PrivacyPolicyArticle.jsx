ef18a7ccd65a47f215cdf775f9c3f323fda6f056
VectorTetra <viktochonov@gmail.com>
2024-02-14 11:20:43 +0200

    Again

--- Page Public contracts and offers/PrivacyPolicyArticle.jsx
+++ Page Public contracts and offers/PrivacyPolicyArticle.jsx
@@ -12,7 +12,7 @@ function PrivacyPolicyArticle(props) {
 								<b>{item.number}</b> {item.text}
 							</p>
 							{
-								(item.sublist.length > 0) ? <PrivacyPolicyArticleSublist sublist={item.sublist}/> : null 
+								(item.sublist !== undefined) ? <PrivacyPolicyArticleSublist sublist={item.sublist}/> : null 
 							}
 						</li>		
 					)
