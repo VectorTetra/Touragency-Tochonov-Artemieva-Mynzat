8b149287ee6d073c8aaf880597973fcb056f3943
VectorTetra <viktochonov@gmail.com>
2024-02-25 19:19:44 +0200

    Apply some paddings and max-width

--- MainPage/jsx-Header/MainPageHeaderContactList.jsx
+++ MainPage/jsx-Header/MainPageHeaderContactList.jsx
@@ -1,6 +1,6 @@
 function MainPageHeaderContactList(props){
 	return(
-		<div style={{display:"flex", flexDirection:"column"}}>
+		<div style={{display:"flex", flexDirection:"column",padding:"0 25px"}}>
 			<div id="mainPageHeaderContactList">
 				{
 					props.workSchedule.map(item=> <div className="contactItem-1">{item}</div>)
