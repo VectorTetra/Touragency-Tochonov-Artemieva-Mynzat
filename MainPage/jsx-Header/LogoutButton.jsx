919d3bd7bbf35b983b7bb8fc9f34f4f830abf277
VectorTetra <viktochonov@gmail.com>
2024-02-25 18:33:11 +0200

    Typos was fixed

--- MainPage/jsx-Header/LogoutButton.jsx
+++ MainPage/jsx-Header/LogoutButton.jsx
@@ -2,7 +2,7 @@ function LogoutButton(props){
 	return(
 		<button id="logoutButton">
 			<img className="logoutButtonIcon" src={props.icon.url}/>			
-			<span className="logoutButtonIcon">{props.text}</span>	
+			<span className="logoutButtonCaption">{props.text}</span>	
 		</button>
 	)
 }
