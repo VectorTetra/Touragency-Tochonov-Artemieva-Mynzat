c6006b6b68a10b27d32c1e5ebc8e617b8ea401f9
Axel7 <Axel_Jkit@protonmail.com>
2024-03-03 17:18:36 +0200

    - Added registration page form

---
+++ Page Registration/jsx-registration/RegistrationPage.jsx
@@ -0,0 +1,20 @@
+function RegistrationPage(props){
+    return(
+        <form>
+            <div class="container">
+                <h1>Реєстрація</h1>
+
+                {
+                    props.RegistrationInfo.map(item => 
+                    <registrationItem name={item.name} type={item.type} text={item.Text} placeHolder={item.PlaceHolder}/>)
+                }
+
+                <p>Створюючи аккаунт ви походжуєтесь на <a href="#">Умови та Конфіденційність</a> сайту.</p>
+                <button type="submit" class="registerbtn">Зареєструватися</button>
+            </div>
+            <div class="container signin">
+                <p>Вже є аккаунт? <a href="#">Увійти</a>.</p>
+            </div>
+        </form>
+    )
+}
---
+++ Page Registration/jsx-registration/registrationItem.jsx
@@ -0,0 +1,10 @@
+function registrationItem(props)
+{
+    return(
+        <div class="registration-item">
+            <label for={props.name}><b>{props.text}</b></label>
+            <input type={props.type} placeholder={props.placeHolder} name={props.name} id={props.name} required/>
+            <span class="inv-span"></span>
+        </div>
+    )
+}
