10708b1c51865b9c1cf1f64e7c5251f3ccc1f0e3
VectorTetra <viktochonov@gmail.com>
2024-03-06 17:29:58 +0200

    Again

--- Page Document List/jsx-DocumentList/DocumentListListItemWithSubitems.jsx
+++ Page Document List/jsx-DocumentList/DocumentListListItemWithSubitems.jsx
@@ -1,6 +1,6 @@
 function DocumentListListItemWithSubitems(props) {
 	return(
-		<li key={crypto.randomUUID()}>
+		<li key={crypto.randomUUID()} style={{listStyleImage:"none"}}>
 			<ul className="documentList-subList">
 				{props.data.subList.map(subItem => <li key={crypto.randomUUID()} className="documentList-subListItem">{subItem.text}</li>)}
 			</ul>
