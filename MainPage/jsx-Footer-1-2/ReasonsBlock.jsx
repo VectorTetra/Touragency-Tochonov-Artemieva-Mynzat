b4d4914245542d4066ebf7121cb234e187c4c9d1
Axel7 <Axel_Jkit@protonmail.com>
2024-03-08 20:34:16 +0200

    - fonts fix

--- MainPage/jsx-Footer-1-2/ReasonsBlock.jsx
+++ MainPage/jsx-Footer-1-2/ReasonsBlock.jsx
@@ -3,8 +3,8 @@ function ReasonsBlock(props)
     return(
         <div class="reasons-info">
                 <span class="dot"><img src={props.ImgUrl} style={{marginTop: '10px'}}/></span> 
-                <h3>{props.header}</h3>
-                <p>{props.parag}</p>
+                <h3 class="info-caption">{props.header}</h3>
+                <p class="info-paragraph">{props.parag}</p>
             </div>
     )
 }
