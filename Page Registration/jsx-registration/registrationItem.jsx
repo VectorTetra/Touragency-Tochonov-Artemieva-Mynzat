3ebd98be34f3ad997fe887bd9ae36030fda8c35d
Axel7 <Axel_Jkit@protonmail.com>
2024-03-09 21:19:01 +0200

    - Зміни до форми реєстрації. ПІБ розділенно на Призвіще, Ім'я, По-батькові окремо - Додани файли та дерикторії сторінки аутинтифікації

--- Page Registration/jsx-registration/registrationItem.jsx
+++ Page Registration/jsx-registration/registrationItem.jsx
@@ -3,7 +3,14 @@ function RegistrationItem(props)
     return(
         <div class="registration-item">
             <label for={props.name}><b>{props.text}</b></label>
-            <input type={props.type} placeholder={props.placeHolder} name={props.name} id={props.name} required/>
+            {
+                (props.name === "middlename" ?
+                    <input type={props.type} placeholder={props.placeHolder} name={props.name} id={props.name}/>
+                    :
+                    <input type={props.type} placeholder={props.placeHolder} name={props.name} id={props.name} required/>
+                )
+                
+            }
             <span class="inv-span"></span>
         </div>
     )
