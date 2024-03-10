c460f785ae96a3386b2797b0082f17e81736a496
Axel7 <Axel_Jkit@protonmail.com>
2024-03-10 16:44:57 +0200

    - fix of form

--- Page Registration/jsx-registration/registrationItem.jsx
+++ Page Registration/jsx-registration/registrationItem.jsx
@@ -2,7 +2,7 @@ function RegistrationItem(props)
 {
     return(
         <div class="registration-item">
-            <label for={props.name}><b>{props.text}</b></label>
+            <label for={props.name}><b>{props.text}</b></label><br/>
             {
                 (props.name === "middlename" ?
                     <input type={props.type} placeholder={props.placeHolder} name={props.name} id={props.name}/>
