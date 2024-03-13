2b44aab9085a23166e1a9730c4640efe97e6e055
VectorTetra <viktochonov@gmail.com>
2024-03-13 21:06:39 +0200

    Again

--- Page Admin Panel/jsx-AdminPanel/DropdownCaption.jsx
+++ Page Admin Panel/jsx-AdminPanel/DropdownCaption.jsx
@@ -1,8 +1,8 @@
 function DropdownCaption(props){
 	return (
-		<div className="dropdownCaption">
-				<img src={props.currentTab.tabIconUrl}  className="dropdownCaptionIcon" />
-				<div className="dropdownCaptionName">{props.currentTab.name}</div>
+		<div className="dropdownCaption" onClick={props.onClick}>
+			<img src={props.currentTab.tabIconUrl}  className="dropdownCaptionIcon" />
+			<div className="dropdownCaptionName">{props.currentTab.name}</div>
 		</div>
 	);
 };
