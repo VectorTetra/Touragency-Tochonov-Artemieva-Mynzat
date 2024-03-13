b5b929407f676ec6184c54223c6a9a1170a4ac61
VectorTetra <viktochonov@gmail.com>
2024-03-13 21:30:28 +0200

    Trying to console DropdownList props

--- Page Admin Panel/jsx-AdminPanel/DropdownList.jsx
+++ Page Admin Panel/jsx-AdminPanel/DropdownList.jsx
@@ -2,13 +2,14 @@ class DropdownList extends React.Component {
 	constructor(props) {
 		super(props);
 		this.sendDataToDropdownCaption = this.sendDataToDropdownCaption.bind(this);
+		console.log("DropdownList",this.props);
 	}
 	sendDataToDropdownCaption(TabInfo) {
 		this.props.sendDataToDropdownCaptionComponent(TabInfo);
 	}
 	render() {
 		return (
-			<div id="dropdownList" style={{ display: this.props.isVisible ? 'relative' : 'none' }}>
+			<div id="dropdownList" style={{ "display": this.props.isVisible ? 'relative' : 'none' }}>
 				{this.props.tabs.map(it => (
 					<DropdownItem iconSrc={it.tabIconUrl} name={it.name} sendDataToDropdownListComponent={this.sendDataToDropdownCaption}/>
 				))}
