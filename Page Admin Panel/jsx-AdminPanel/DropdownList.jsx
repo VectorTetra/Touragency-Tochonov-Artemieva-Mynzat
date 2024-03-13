25c7906a976cedc95762121e9daef2b0640c71b2
VectorTetra <viktochonov@gmail.com>
2024-03-13 21:44:06 +0200

    Again

--- Page Admin Panel/jsx-AdminPanel/DropdownList.jsx
+++ Page Admin Panel/jsx-AdminPanel/DropdownList.jsx
@@ -2,6 +2,7 @@ class DropdownList extends React.Component {
 	constructor(props) {
 		super(props);
 		this.sendDataToDropdownCaption = this.sendDataToDropdownCaption.bind(this);
+		this.state = {isVisible: this.props.isVisible};
 		console.log("DropdownList",this.props);
 	}
 	sendDataToDropdownCaption(TabInfo) {
@@ -9,7 +10,7 @@ class DropdownList extends React.Component {
 	}
 	render() {
 		return (
-			<div id="dropdownList" style={{ "display": this.props.isVisible ? 'relative' : 'none' }}>
+			<div id="dropdownList" style={{ "display": this.state.isVisible ? 'relative' : 'none' }}>
 				{this.props.tabs.map(it => (
 					<DropdownItem iconSrc={it.tabIconUrl} name={it.name} sendDataToDropdownListComponent={this.sendDataToDropdownCaption}/>
 				))}
