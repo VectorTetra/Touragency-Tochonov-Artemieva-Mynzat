b7749bf2c2a754741ef28e4b6536a0d9ebd3ee8c
Axel7 <Axel_Jkit@protonmail.com>
2024-03-14 20:54:41 +0200

    - patterns added to registration form

--- Page Registration/jsx-registration/registrationItem.jsx
+++ Page Registration/jsx-registration/registrationItem.jsx
@@ -5,9 +5,9 @@ function RegistrationItem(props)
             <label for={props.name}><b>{props.text}</b></label><br/>
             {
                 (props.name === "middlename" ?
-                    <input type={props.type} placeholder={props.placeHolder} name={props.name} id={props.name}/>
+                    <input type={props.type} placeholder={props.placeHolder} name={props.name} id={props.name} pattern={props.pattern}/>
                     :
-                    <input type={props.type} placeholder={props.placeHolder} name={props.name} id={props.name} required/>
+                    <input type={props.type} placeholder={props.placeHolder} name={props.name} id={props.name} pattern={props.pattern} required/>
                 )
                 
             }
