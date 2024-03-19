daf29f6beb0bb957e532ee2cecb571fda8f589c3
VectorTetra <viktochonov@gmail.com>
2024-03-20 00:06:20 +0200

    Ще покращення дизайну

--- Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountryListItem.jsx
+++ Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountryListItem.jsx
@@ -1,7 +1,7 @@
 function CountryListItem(props) {
 	return (
 		<div className="countryListItem">
-			<div style={{"display": "flex"}}>
+			<div className="countryListItemStatContainer">
 				<img className="countryListItemFlagImg" src={props.country.FlagUrl} alt={props.country.Name} />
 				<div className="countryListItemNameDiv">{props.country.Name}</div>
 			</div>
