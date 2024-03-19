e1eab7ccc2fba5a03a712db276d20dab62b9f644
VectorTetra <viktochonov@gmail.com>
2024-03-19 22:30:03 +0200

    Додано кнопки редаг / видалення країни

--- Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountryListItem.jsx
+++ Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountryListItem.jsx
@@ -3,6 +3,11 @@ function CountryListItem(props) {
 	<div className="countryListItem">
 		<img className="countryListItemFlagImg" src={props.country.FlagUrl} alt={props.country.Name} />
 		<div className="countryListItemNameDiv">{props.country.Name}</div>
+		<form action="post" className="countryListItemFormButtonBar">
+			<input type="hidden" name="countryId" value={props.key} />
+			<button type="submit" className="form-editbutton">Змінити</button>
+			<button type="submit" className="form-clearbutton">Видалити</button>
+		</form>
 	</div>
   );
 }
