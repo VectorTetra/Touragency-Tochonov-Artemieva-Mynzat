b88eab9bad19cfacc31cfa832f8b6ffd0d6931b5
VectorTetra <viktochonov@gmail.com>
2024-03-19 22:55:04 +0200

    Намагаюся зробити кращу компоновку списку країн

--- Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountryListItem.jsx
+++ Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountryListItem.jsx
@@ -1,13 +1,15 @@
 function CountryListItem(props) {
-  return (
-	<div className="countryListItem">
-		<img className="countryListItemFlagImg" src={props.country.FlagUrl} alt={props.country.Name} />
-		<div className="countryListItemNameDiv">{props.country.Name}</div>
-		<form action="post" className="countryListItemFormButtonBar">
-			<input type="hidden" name="countryId" value={props.key} />
-			<button type="submit" className="form-editbutton">Змінити</button>
-			<button type="submit" className="form-clearbutton">Видалити</button>
-		</form>
-	</div>
-  );
+	return (
+		<div className="countryListItem">
+			<div>
+				<img className="countryListItemFlagImg" src={props.country.FlagUrl} alt={props.country.Name} />
+				<div className="countryListItemNameDiv">{props.country.Name}</div>
+			</div>
+			<form action="post" className="countryListItemFormButtonBar">
+				<input type="hidden" name="countryId" value={props.key} />
+				<button type="submit" className="form-editbutton">Змінити</button>
+				<button type="submit" className="form-clearbutton">Видалити</button>
+			</form>
+		</div>
+	);
 }
