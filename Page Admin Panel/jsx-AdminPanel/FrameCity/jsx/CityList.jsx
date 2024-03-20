0e26277cc58475c5fc4256c3d7a394a856697891
VectorTetra <viktochonov@gmail.com>
2024-03-20 17:20:31 +0200

    Намагаюся визначити, чому не виводиться CityList

--- Page Admin Panel/jsx-AdminPanel/FrameCity/jsx/CityList.jsx
+++ Page Admin Panel/jsx-AdminPanel/FrameCity/jsx/CityList.jsx
@@ -2,6 +2,9 @@ function CityList(props){
 	return (
 		<div id="cityList">
 			{
+				console.log(props.countries)
+			}
+			{
 				props.countries.map((country, index) => {
 					country.Cities.map((city, index) => {
 						return <CityListItem key={crypto.randomUUID()} country={country} city={city}/>
