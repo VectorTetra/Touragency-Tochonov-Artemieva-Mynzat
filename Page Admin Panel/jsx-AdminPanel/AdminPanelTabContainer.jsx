cb8a62eeaf81b589d287cb4c8a926b4476489743
VectorTetra <viktochonov@gmail.com>
2024-03-20 10:52:47 +0200

    Again

--- Page Admin Panel/jsx-AdminPanel/AdminPanelTabContainer.jsx
+++ Page Admin Panel/jsx-AdminPanel/AdminPanelTabContainer.jsx
@@ -1,26 +1,25 @@
 import React, { Suspense } from 'react';
 class AdminPanelTabContainer extends React.Component {
-	constructor(props) 
-	{
+	constructor(props) {
 		super(props);
-		this.state = {name: this.props.tabs[0].name, tabIconUrl: this.props.tabs[0].tabIconUrl};
+		this.state = { name: this.props.tabs[0].name, tabIconUrl: this.props.tabs[0].tabIconUrl };
 	}
 	render() {
 		// Знайти вкладку "Країни"
 		let countryTab = this.props.tabs.find(el => el.name === "Країни");
 
 		return (
-			<Suspense fallback={<Loading />}>
-				<div id="adminPanelTabContainer">
-					{
-						(this.state.name === "Країни") ? (
-							// Передайте його як проп в FrameCountry
+			<div id="adminPanelTabContainer">
+				{
+					(this.state.name === "Країни") ? (
+						// Передайте його як проп в FrameCountry
+						<Suspense fallback={<Loading />}>
 							<FrameCountry tab={countryTab} />
-						) : null
-					}
-				</div>
-        	</Suspense>
-			
+						</Suspense>
+					) : null
+				}
+			</div>
+
 		);
 	}
 }
--- Page Admin Panel/jsx-AdminPanel/Loading.jsx
+++ Page Admin Panel/jsx-AdminPanel/Loading.jsx
@@ -1,3 +1,4 @@
+import React, { Suspense } from 'react';
 function Loading (props){
     return (
         <div className="loading">
