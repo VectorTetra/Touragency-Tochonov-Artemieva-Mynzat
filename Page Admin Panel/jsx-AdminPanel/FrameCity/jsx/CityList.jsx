c8871d5fe77127046487067052c50128f8edf9f1
VectorTetra <viktochonov@gmail.com>
2024-03-20 17:24:59 +0200

    Виправлено нестачу return

--- Page Admin Panel/jsx-AdminPanel/FrameCity/jsx/CityList.jsx
+++ Page Admin Panel/jsx-AdminPanel/FrameCity/jsx/CityList.jsx
@@ -2,11 +2,8 @@ function CityList(props){
 	return (
 		<div id="cityList">
 			{
-				console.log(props.countries)
-			}
-			{
 				props.countries.map((country, index) => {
-					country.Cities.map((city, index) => {
+					return country.Cities.map((city, index) => {
 						return <CityListItem key={crypto.randomUUID()} country={country} city={city}/>
 					})
 				})
