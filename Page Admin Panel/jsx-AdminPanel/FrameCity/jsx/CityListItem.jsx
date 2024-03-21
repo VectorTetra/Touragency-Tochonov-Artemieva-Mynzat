ef572b609c20abe3996104c6c45e96c569fd371f
VectorTetra <viktochonov@gmail.com>
2024-03-21 13:18:43 +0200

    Виправлено бекграунд на кнопці видаленння, встановлено вирівнювання назв країн і міст по лівому краю

--- Page Admin Panel/jsx-AdminPanel/FrameCity/jsx/CityListItem.jsx
+++ Page Admin Panel/jsx-AdminPanel/FrameCity/jsx/CityListItem.jsx
@@ -4,8 +4,8 @@ function CityListItem(props) {
 			<div className="cityListItemStatContainer">
 				<img className="countryListItemFlagImg" src={props.country.FlagUrl} alt={props.country.Name} />
 				<div className="cityListItemStringStatContainer">
-					<div className="countryListItemNameDiv">Країна: {props.country.Name}</div>
-					<div className="countryListItemNameDiv">Місто: {props.city.Name}</div>
+					<div className="cityListItemNameDiv">Країна: {props.country.Name}</div>
+					<div className="cityListItemNameDiv">Місто: {props.city.Name}</div>
 				</div>
 			</div>
 			<form action="post" className="cityListItemFormButtonBar">
