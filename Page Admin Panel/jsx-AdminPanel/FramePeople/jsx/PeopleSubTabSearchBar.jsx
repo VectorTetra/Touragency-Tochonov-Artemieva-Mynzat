69da8a8f5828fd00f2a6d1ad189d77d6a6b46658
VectorTetra <viktochonov@gmail.com>
2024-04-21 16:56:20 +0300

    Виправлено помилку з необ'явленим обробником

--- Page Admin Panel/jsx-AdminPanel/FramePeople/jsx/PeopleSubTabSearchBar.jsx
+++ Page Admin Panel/jsx-AdminPanel/FramePeople/jsx/PeopleSubTabSearchBar.jsx
@@ -20,14 +20,20 @@ function PeopleSubTabSearchBar(props){
 	{
 		setInputMiddlenameValue(event.target.value);
 	}
+	const handleInputSearchById = (event) => 
+	{
+	}
+	const handleInputSearchByOther = (event) => 
+	{
+	}
 	return (
 		<form className="EditFormRow searchBarRow" method="post">
 			<input type="number" min={0} className="EditFormInput" name="Id" value={inputIdValue} onInput={handleInputIdValue} placeholder="Введіть ID" />
 			<input className="EditFormInput" name="Firstname" value={inputFirstnameValue} onInput={handleInputFirstnameValue} placeholder="Введіть ім'я" />
 			<input className="EditFormInput" name="Lastname" value={inputLastnameValue} onInput={handleInputLastnameValue} placeholder="Введіть прізвище" />
 			<input className="EditFormInput" name="Middlename" value={inputMiddlenameValue} onInput={handleInputMiddlenameValue} placeholder="Введіть по-батькові (необов'язково)" />
-			<input type="submit" className="buttonSearchCity" name="buttonSearchById" value="Пошук по ID" onClick={handleInput} />
-			<input type="submit" className="buttonSearchCity" name="buttonSearchByOther" value="Пошук за П.І.Б" onClick={handleInput} />
+			<input type="submit" className="buttonSearchCity" name="buttonSearchById" value="Пошук по ID" onClick={handleInputSearchById} />
+			<input type="submit" className="buttonSearchCity" name="buttonSearchByOther" value="Пошук за П.І.Б" onClick={handleInputSearchByOther} />
 		</form>
 	);
 }
