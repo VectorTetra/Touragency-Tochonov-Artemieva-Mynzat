7921c20f0421d66c8af4fe807f67c28413266f01
VectorTetra <viktochonov@gmail.com>
2024-04-21 15:30:06 +0300

    Намагаюся зробити покращений стиль таблиці з персонами

--- Page Admin Panel/jsx-AdminPanel/FramePeople/jsx/PeopleSubTabListItem.jsx
+++ Page Admin Panel/jsx-AdminPanel/FramePeople/jsx/PeopleSubTabListItem.jsx
@@ -1,10 +1,19 @@
 function PeopleSubTabListItem({person}){
 	return (
 		<div className="peopleSubTabListItem">
-			<p>ID: {person.id}</p>
-			<p>Ім'я: {person.firstName}</p>
-			<p>Прізвище: {person.lastName}</p>
-			<p>По-батькові: {person.middleName}</p>
+			<div className="peopleSubTabListItemStatContainer">
+				<p>ID: {person.id}</p>
+			</div>
+			<div className="peopleSubTabListItemStatContainer">
+				<p>Ім'я: {person.firstName}</p>
+				<p>Прізвище: {person.lastName}</p>
+				<p>По-батькові: {person.middleName}</p>
+			</div>
+			<form action="post" className="cityListItemFormButtonBar">
+				<input type="hidden" name="countryId" value={person.id} />
+				<button type="submit" className="form-editbutton-small"></button>
+				<button type="submit" className="form-clearbutton-small"></button>
+			</form>
 		</div>
 	);	
 }
