f69ba120356e70a91cc6b3b71b1f390eb75664e1
VectorTetra <viktochonov@gmail.com>
2024-04-25 19:50:14 +0300

    Змінено назви змінних

--- Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountryListItem.jsx
+++ Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountryListItem.jsx
@@ -2,11 +2,11 @@ function CountryListItem(props) {
 	return (
 		<div className="countryListItem">
 			<div className="countryListItemStatContainer">
-				<img className="countryListItemFlagImg" src={props.country.FlagUrl} alt={props.country.Name} />
-				<div className="countryListItemNameDiv">{props.country.Name}</div>
+				<img className="countryListItemFlagImg" src={props.country.flagUrl} alt={props.country.name} />
+				<div className="countryListItemNameDiv">{props.country.name}</div>
 			</div>
 			<form action="post" className="countryListItemFormButtonBar">
-				<input type="hidden" name="countryId" value={props.key} />
+				<input type="hidden" name="countryId" value={props.country.id} />
 				<button type="submit" className="form-editbutton">Змінити</button>
 				<button type="submit" className="form-clearbutton">Видалити</button>
 			</form>
