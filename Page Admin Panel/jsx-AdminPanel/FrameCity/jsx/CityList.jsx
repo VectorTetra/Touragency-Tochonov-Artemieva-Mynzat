c448773f93eea62c7df48b2eed677d895a109a6d
VectorTetra <viktochonov@gmail.com>
2024-04-25 20:44:08 +0300

    Змінено синтаксис CityList

--- Page Admin Panel/jsx-AdminPanel/FrameCity/jsx/CityList.jsx
+++ Page Admin Panel/jsx-AdminPanel/FrameCity/jsx/CityList.jsx
@@ -3,7 +3,7 @@ function CityList(props){
 	return (
 		<div id="cityList">
 			{
-				props.cities?.map((city, index) => {
+				props.cities.map((city, index) => {
 					return <CityListItem key={crypto.randomUUID()} country={props.countries.find(country => country.Name === city.CountryName)} city={city}/>
 				})
 			}
