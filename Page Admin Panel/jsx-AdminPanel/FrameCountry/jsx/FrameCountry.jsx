33d55139ea18d5313a8f2d5ca72eeb3fb3f5cd57
VectorTetra <viktochonov@gmail.com>
2024-04-25 17:19:51 +0300

    Спроба переробити отримання країн в FrameCountry із пропсів на отримання із WebApi

--- Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/FrameCountry.jsx
+++ Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/FrameCountry.jsx
@@ -1,13 +1,40 @@
-function FrameCountry(props){
-	const [quantity, setQuantity] = React.useState(props.tab.countries.length);
-	const [countries, setCountries] = React.useState(props.tab.countries);
+// function FrameCountry(props){
+// 	const [quantity, setQuantity] = React.useState(props.tab.countries.length);
+// 	const [countries, setCountries] = React.useState(props.tab.countries);
 
-	return (
-		<div id="frameCountry">
-			<FrameCountryHeader quantity={quantity} />
-			<CountryEditForm />
-			<CountrySearchBar tab={props.tab} setQuantity={setQuantity} setCountries={setCountries} />
-			<CountryList countries={countries} />
-		</div>
-	);
+// 	return (
+// 		<div id="frameCountry">
+// 			<FrameCountryHeader quantity={quantity} />
+// 			<CountryEditForm />
+// 			<CountrySearchBar tab={props.tab} setQuantity={setQuantity} setCountries={setCountries} />
+// 			<CountryList countries={countries} />
+// 		</div>
+// 	);
+// };
+function FrameCountry(props){
+    const [quantity, setQuantity] = React.useState(0);
+    const [countries, setCountries] = React.useState([]);
+
+    React.useEffect(() => {
+        $.ajax({
+            url: 'http://26.162.95.213:22546/api/Country', // Замініть на ваш URL API
+            data: { SearchParameter: 'GetAll' },
+            success: function(data) {
+                setCountries(data);
+                setQuantity(data.length);
+            },
+            error: function(error) {
+                console.error('Помилка при отриманні даних', error);
+            }
+        });
+    }, []); // Пустий масив означає, що цей ефект буде виконуватися тільки при монтуванні компонента
+
+    return (
+        <div id="frameCountry">
+            <FrameCountryHeader quantity={quantity} />
+            <CountryEditForm />
+            <CountrySearchBar tab={props.tab} setQuantity={setQuantity} setCountries={setCountries} />
+            <CountryList countries={countries} />
+        </div>
+    );
 };
