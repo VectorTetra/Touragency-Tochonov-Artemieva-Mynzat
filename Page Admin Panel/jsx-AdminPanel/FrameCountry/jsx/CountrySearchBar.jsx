a0d2778799188057ac252a2f958af8422c3cffb6
VectorTetra <viktochonov@gmail.com>
2024-04-26 16:02:43 +0300

    Спробую додати кнопку для пошуку

--- Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountrySearchBar.jsx
+++ Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountrySearchBar.jsx
@@ -1,25 +1,23 @@
 function CountrySearchBar(props) {
 	const context = React.useContext(window.FrameCountryContext);
-	const [localInput, setLocalInput] = React.useState(''); // Локальний стан для поля вводу
 
 	const handleInput = (event) => {
-		setLocalInput(event.target.value); // Оновлюємо локальний стан при вводі
+		context.setInputName(event.target.value);
 	};
 
 	React.useEffect(() => {
-		context.setInputName(localInput); // Оновлюємо глобальний стан, коли локальний стан змінюється
-		if (localInput === "") {
+		if (context.inputName === "") {
 			context.Get200Last();
 			return;
 		}
 		else {
 			context.GetByName();
 		}
-	}, [localInput]); // Залежність від localInput, а не context.inputName
-
+	}, [context.inputName]);
 	return (
 		<div className="EditFormRow searchBarRow">
-			<input className="EditFormInput" name="searchBar" value={localInput} placeholder="Введіть назву країни" onInput={handleInput} />
+			<input className="EditFormInput" name="searchBar" value={context.inputName} placeholder="Введіть назву країни"/>
+			<input type="submit" className="buttonSearchCity" name="buttonSearchCity" value="Пошук" onClick={handleInput} />
 		</div>
 	);
 };
