60b6bf1e09db6b6f4de8b112338a864e7654113a
VectorTetra <viktochonov@gmail.com>
2024-04-28 15:42:05 +0300

    Прибрано контекстні змінні з форми редагування

--- Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountryEditForm.jsx
+++ Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountryEditForm.jsx
@@ -1,21 +1,9 @@
 function CountryEditForm(props) {
 	const context = React.useContext(window.FrameCountryContext);
-	const [countryName, setCountryName] = React.useState('');
-	const [flagUrl, setFlagUrl] = React.useState('');
-
-	const handleCountryNameChange = (event) => {
-		setCountryName(event.target.value);
-	};
-
-	const handleFlagUrlChange = (event) => {
-		setFlagUrl(event.target.value);
-	};
 
 	const handleSubmit = (event) => {
-		event.preventDefault();
-		context.setDtoName(countryName);
-		context.setDtoFlagUrl(flagUrl);		
-		if(context.dtoId === 0) 
+		event.preventDefault();	
+		if($("#EditFormInputCountryId").val() === 0) 
 		{
 			context.PostCountry();
 		}
@@ -28,14 +16,14 @@ function CountryEditForm(props) {
 
 	return (
 		<form name="countryEditForm" id="countryEditForm" style={{ border: '1px solid black', borderRadius: '5px' }} onSubmit={handleSubmit}>
-			<input id="EditFormInputCountryId" type="hidden" name="countryIdInput" value={context.dtoId}/>
+			<input id="EditFormInputCountryId" type="hidden" name="countryIdInput" value={0}/>
 			<div className="EditFormRow">
 				<div>Назва країни:</div>
-				<input id="EditFormInputCountryName" className="EditFormInput" name="countryNameInput" required onChange={handleCountryNameChange} />
+				<input id="EditFormInputCountryName" className="EditFormInput" name="countryNameInput" required/>
 			</div>
 			<div className="EditFormRow">
 				<div>URL до зображення прапора країни:</div>
-				<input id="EditFormInputCountryUrlFlag" className="EditFormInput" name="urlFlagInput" required onChange={handleFlagUrlChange} />
+				<input id="EditFormInputCountryUrlFlag" className="EditFormInput" name="urlFlagInput" required/>
 			</div>
 			<div className="EditFormRowButtons" style={{ margin: '15px 0 15px 15px' }}>
 				<input type="submit" id="userFormSubmit" className="form-savebutton" value="Зберегти"></input>
