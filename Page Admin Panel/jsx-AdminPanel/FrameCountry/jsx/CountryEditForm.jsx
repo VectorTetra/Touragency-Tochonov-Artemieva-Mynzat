2ac45550036edd37edb754937eb23182ef186fa1
VectorTetra <viktochonov@gmail.com>
2024-04-28 14:15:46 +0300

    Спроба використовувати state для POST і PUT

--- Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountryEditForm.jsx
+++ Page Admin Panel/jsx-AdminPanel/FrameCountry/jsx/CountryEditForm.jsx
@@ -1,9 +1,20 @@
 function CountryEditForm(props) {
 	const context = React.useContext(window.FrameCountryContext);
+	const [countryName, setCountryName] = React.useState('');
+	const [flagUrl, setFlagUrl] = React.useState('');
+
+	const handleCountryNameChange = (event) => {
+		setCountryName(event.target.value);
+	};
+
+	const handleFlagUrlChange = (event) => {
+		setFlagUrl(event.target.value);
+	};
+
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		context.setDtoName(event.target.elements.countryNameInput.value);
-		context.setDtoFlagUrl(event.target.elements.urlFlagInput.value);		
+		context.setDtoName(countryName);
+		context.setDtoFlagUrl(flagUrl);		
 		if(context.dtoId === 0) 
 		{
 			context.PostCountry();
@@ -14,16 +25,17 @@ function CountryEditForm(props) {
 		}
 		
 	};
+
 	return (
 		<form name="countryEditForm" id="countryEditForm" style={{ border: '1px solid black', borderRadius: '5px' }} onSubmit={handleSubmit}>
 			<input type="hidden" name="countryIdInput" value={context.dtoId}/>
 			<div className="EditFormRow">
 				<div>Назва країни:</div>
-				<input className="EditFormInput" name="countryNameInput" required />
+				<input className="EditFormInput" name="countryNameInput" required onChange={handleCountryNameChange} />
 			</div>
 			<div className="EditFormRow">
 				<div>URL до зображення прапора країни:</div>
-				<input className="EditFormInput" name="urlFlagInput" required />
+				<input className="EditFormInput" name="urlFlagInput" required onChange={handleFlagUrlChange} />
 			</div>
 			<div className="EditFormRowButtons" style={{ margin: '15px 0 15px 15px' }}>
 				<input type="submit" id="userFormSubmit" className="form-savebutton" value="Зберегти"></input>
