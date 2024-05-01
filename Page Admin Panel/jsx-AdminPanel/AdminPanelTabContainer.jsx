cb8b36a471c4ece7fec4b06664af9e526bf9cb77
VectorTetra <viktochonov@gmail.com>
2024-05-01 15:44:57 +0300

    Revert "Добавлена кнопка для перехода на главную страницу сайта Магія Мандрів…"
    This reverts commit 43bc606cfe1c7ecfee5210820020504653a8c3db.

--- Page Admin Panel/jsx-AdminPanel/AdminPanelTabContainer.jsx
+++ Page Admin Panel/jsx-AdminPanel/AdminPanelTabContainer.jsx
@@ -1,19 +1,25 @@
-
 class AdminPanelTabContainer extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = { name: this.props.tabs[0].name, tabIconUrl: this.props.tabs[0].tabIconUrl };
 	}
-
 	render() {
-		let myTab = this.props.tabs.find(el => el.name === this.state.name);
+		// Знайти вкладку "Країни"
+		let countryTab = this.props.tabs.find(el => el.name === "Країни");
 
 		return (
 			<div id="adminPanelTabContainer">
-				{(this.state.name === "Країни") ? (<FrameCountry tab={myTab} />) : null}
-				{(this.state.name === "Готелі") ? (<FrameHotel tab={myTab} />) : null}
-				{(this.state.name === "Відгуки") ? (<FrameFeedback tab={myTab} />) : null}
-				{(this.state.name === "Міста") ? (<FrameCity tab={countryTab} />) : null}
+				{
+					(this.state.name === "Країни") ? (
+						// Передайте його як проп в FrameCountry
+							<FrameCountry tab={countryTab} />
+					) : null
+				}
+				{
+					(this.state.name === "Міста") ? (
+							<FrameCity tab={countryTab} />
+					) : null
+				}
 			</div>
 
 		);
