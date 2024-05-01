9c7d56030cf1a509f4222c34cb9595fe7aa1da3d
artelena1906 <74003242+artelena1906@users.noreply.github.com>
2024-05-01 14:59:07 +0300

    Добавлена кнопка для перехода на главную страницу сайта Магія Мандрів

--- Page Admin Panel/jsx-AdminPanel/AdminPanelTabContainer.jsx
+++ Page Admin Panel/jsx-AdminPanel/AdminPanelTabContainer.jsx
@@ -1,25 +1,19 @@
+
 class AdminPanelTabContainer extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = { name: this.props.tabs[0].name, tabIconUrl: this.props.tabs[0].tabIconUrl };
 	}
+
 	render() {
-		// Знайти вкладку "Країни"
-		let countryTab = this.props.tabs.find(el => el.name === "Країни");
+		let myTab = this.props.tabs.find(el => el.name === this.state.name);
 
 		return (
 			<div id="adminPanelTabContainer">
-				{
-					(this.state.name === "Країни") ? (
-						// Передайте його як проп в FrameCountry
-							<FrameCountry tab={countryTab} />
-					) : null
-				}
-				{
-					(this.state.name === "Міста") ? (
-							<FrameCity tab={countryTab} />
-					) : null
-				}
+				{(this.state.name === "Країни") ? (<FrameCountry tab={myTab} />) : null}
+				{(this.state.name === "Готелі") ? (<FrameHotel tab={myTab} />) : null}
+				{(this.state.name === "Відгуки") ? (<FrameFeedback tab={myTab} />) : null}
+				{(this.state.name === "Міста") ? (<FrameCity tab={countryTab} />) : null}
 			</div>
 
 		);
--- Page Admin Panel/jsx-AdminPanel/DropdownItem.jsx
+++ Page Admin Panel/jsx-AdminPanel/DropdownItem.jsx
@@ -1,14 +1,32 @@
-function DropdownItem(props){
-	function sendDataToDropdownList(){
-		props.sendDataToDropdownListComponent({
-			"name":props.name,
-			"tabIconUrl":props.iconSrc
-		});
-	}
-	return(
-		<div className="dropdownItem" onClick={sendDataToDropdownList}>
-			<img src={props.iconSrc} alt={props.name} className="dropdownItemIcon" />
-			<div className="dropdownItemName">{props.name}</div>
-		</div>
-	)
+// function DropdownItem(props){
+// 	function sendDataToDropdownList(){
+// 		props.sendDataToDropdownListComponent({
+// 			"name":props.name,
+// 			"tabIconUrl":props.iconSrc
+// 		});
+// 	}
+// 	return(
+// 		<div className="dropdownItem" onClick={sendDataToDropdownList}>
+// 			<img src={props.iconSrc} alt={props.name} className="dropdownItemIcon" />
+// 			<div className="dropdownItemName">{props.name}</div>
+// 		</div>
+// 	)
+// }
+
+function DropdownItem(props) {
+    return (
+        <div className="dropdownItem">
+            {props.name === "МАГІЯ МАНДРІВ" ? (
+                <a href="../../MainPage/index.html">
+                    <img src={props.iconSrc} alt={props.name} className="dropdownItemIcon" />
+                    <span className="dropdownItemNamePageHead">{props.name}</span>
+                </a>
+            ) : (
+                <div onClick={props.sendDataToDropdownListComponent}>
+                    <img src={props.iconSrc} alt={props.name} className="dropdownItemIcon" />
+                    <span className="dropdownItemName">{props.name}</span>
+                </div>
+            )}
+        </div>
+    );
 }
