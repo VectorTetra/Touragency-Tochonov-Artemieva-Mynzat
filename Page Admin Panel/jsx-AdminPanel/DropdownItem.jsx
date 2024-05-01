5546f674d12dd997b11479816d8d3e34f1d59b63
VectorTetra <viktochonov@gmail.com>
2024-05-01 16:48:00 +0300

    Змінено рендер DropdownItem

--- Page Admin Panel/jsx-AdminPanel/DropdownItem.jsx
+++ Page Admin Panel/jsx-AdminPanel/DropdownItem.jsx
@@ -1,38 +1,41 @@
-// function DropdownItem(props){
-// 	function sendDataToDropdownList(){
-// 		props.sendDataToDropdownListComponent({
-// 			"name":props.name,
-// 			"tabIconUrl":props.iconSrc
-// 		});
-// 	}
-// 	return(
-// 		<div className="dropdownItem" onClick={sendDataToDropdownList}>
-// 			<img src={props.iconSrc} alt={props.name} className="dropdownItemIcon" />
-// 			<div className="dropdownItemName">{props.name}</div>
-// 		</div>
-// 	)
-// }
-
-function DropdownItem(props) {
+function DropdownItem(props){
 	function sendDataToDropdownList(){
 		props.sendDataToDropdownListComponent({
 			"name":props.name,
 			"tabIconUrl":props.iconSrc
 		});
 	}
-    return (
-        <div className="dropdownItem">
-            {props.name === "МАГІЯ МАНДРІВ" ? (
-                <a href="../../MainPage/index.html">
-                    <img src={props.iconSrc} alt={props.name} className="dropdownItemIcon" />
-                    <span className="dropdownItemNamePageHead">{props.name}</span>
-                </a>
-            ) : (
-                <div onClick={props.sendDataToDropdownListComponent}>
-                    <img src={props.iconSrc} alt={props.name} className="dropdownItemIcon" />
-                    <span className="dropdownItemName">{props.name}</span>
-                </div>
-            )}
-        </div>
-    );
+	function returnToMainPage(){
+		window.location.href = "../../MainPage/index.html";
+	}
+	return(
+		<div className="dropdownItem" onClick={props.name === "МАГІЯ МАНДРІВ" ? returnToMainPage : sendDataToDropdownList}>
+			<img src={props.iconSrc} alt={props.name} className="dropdownItemIcon" />
+			<div className="dropdownItemName">{props.name}</div>
+		</div>
+	)
 }
+
+// function DropdownItem(props) {
+// 	function sendDataToDropdownList(){
+// 		props.sendDataToDropdownListComponent({
+// 			"name":props.name,
+// 			"tabIconUrl":props.iconSrc
+// 		});
+// 	}
+//     return (
+//         <div className="dropdownItem">
+//             {props.name === "МАГІЯ МАНДРІВ" ? (
+//                 <a href="../../MainPage/index.html">
+//                     <img src={props.iconSrc} alt={props.name} className="dropdownItemIcon" />
+//                     <span className="dropdownItemNamePageHead">{props.name}</span>
+//                 </a>
+//             ) : (
+//                 <div onClick={props.sendDataToDropdownListComponent}>
+//                     <img src={props.iconSrc} alt={props.name} className="dropdownItemIcon" />
+//                     <span className="dropdownItemName">{props.name}</span>
+//                 </div>
+//             )}
+//         </div>
+//     );
+// }
