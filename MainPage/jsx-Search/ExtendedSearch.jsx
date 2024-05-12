00327897726d3f6c07ea50083f3a9f54e53a6397
Axel7 <Axel_Jkit@protonmail.com>
2024-05-12 21:09:36 +0300

    - search changes

--- MainPage/jsx-Search/ExtendedSearch.jsx
+++ MainPage/jsx-Search/ExtendedSearch.jsx
@@ -3,7 +3,7 @@ function ExtendedSearch(props)
     return(
         <div class="extended-search">
             <ExtSrcColumn1 SearchBarData={props.SearchBarData}></ExtSrcColumn1>
-            <ExtSrcColumn2 SearchBarData={props.SearchBarData}></ExtSrcColumn2>
+            {/* <ExtSrcColumn2 SearchBarData={props.SearchBarData}></ExtSrcColumn2> */}
             <ExtSrcColumn3 SearchBarData={props.SearchBarData}></ExtSrcColumn3>
         </div>
     )
